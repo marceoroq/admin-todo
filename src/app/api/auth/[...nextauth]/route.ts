13d64043e16474c9316fd6c15cabebafbabe39fb
Chelo <chelo@Chelos-MacBook-Air.local>
2025-06-28 19:56:05 -0300

    feat: connect NextAuth to database

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,11 @@ import NextAuth from "next-auth"
 import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
 
+import prisma from "@/lib/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || '',
